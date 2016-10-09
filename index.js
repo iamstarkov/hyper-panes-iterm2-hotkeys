5416813c6f27fb4d33a99f87be9da8740a2f98a2
Vladimir Starkov <iamstarkov@gmail.com>
2016-10-09 18:40:42 +0200

    update menu with new hotkeys

--- index.js
+++ index.js
@@ -30,8 +30,33 @@ const addDefaultHotkeys = config => merge(config, {
   })
 });
 
+const decorateMenu = menu => menu.map(menuItem => {
+  if (menuItem.role !== 'window') {
+    return menuItem;
+  }
+  const newMenuItem = Object.assign({}, menuItem);
+  newMenuItem.submenu = newMenuItem.submenu.map(submenuItem => {
+    const newSubmmenuItem = Object.assign({}, submenuItem);
+    if (newSubmmenuItem.label === 'Show Previous Tab') {
+      newSubmmenuItem.accelerator = 'Ctrl+Shift+Tab';
+    }
+    if (newSubmmenuItem.label === 'Show Next Tab') {
+      newSubmmenuItem.accelerator = 'Ctrl+Tab';
+    }
+    if (newSubmmenuItem.label === 'Select Previous Pane') {
+      newSubmmenuItem.accelerator = 'CmdOrCtrl+Alt+Left';
+    }
+    if (newSubmmenuItem.label === 'Select Next Pane') {
+      newSubmmenuItem.accelerator = 'CmdOrCtrl+Alt+Right';
+    }
+    return newSubmmenuItem;
+  });
+  return newMenuItem;
+});
+
 module.exports = {
   onApp: onApp,
   onWindow: registerWindow,
-  decorateConfig: addDefaultHotkeys
+  decorateConfig: addDefaultHotkeys,
+  decorateMenu: decorateMenu,
 }
